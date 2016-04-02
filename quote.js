e5148a5f0fb18d0ed7d241d278c5fd7490dfe9a8
Jaren831 <jaren.lynchmobb@gmail.com>
2016-04-02 15:42:08 -0700

    Before using jquery

--- quote.js
+++ quote.js
@@ -7,7 +7,7 @@ function set() {
   var quote_count = Math.floor(Math.random()*quotes.length);
   var color_count = Math.floor(Math.random()*colors.length);
   document.getElementById("quote").innerHTML = quotes[quote_count][1];
-  document.getElementById("wik").innerHTML = quotes[quote_count][0];
+  document.getElementById("author").innerHTML = "- " + quotes[quote_count][0];
   document.getElementsByClassName("btn")[0].style.backgroundColor = colors[color_count];
   document.getElementsByClassName("btn")[1].style.backgroundColor = colors[color_count];
   document.getElementsByClassName("btn")[2].style.backgroundColor = colors[color_count];
