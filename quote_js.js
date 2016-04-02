4a9140d4e229ab35f0261167e5d622fe1b7b78bb
Jaren831 <jaren.lynchmobb@gmail.com>
2016-04-01 21:05:54 -0700

    First Commit

---
+++ quote_js.js
@@ -0,0 +1,16 @@
+var quotes = [[" Henry Ward Beecher", "Hold yourself responsible for a higher standard than anybody expects of you. Never excuse yourself."], [" Napoleon Bonaparte", "Glory is fleeting, but obscurity is forever."], [" Bertrand Russell", "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts."]];
+var colors = ['#16a085', '#27ae60', '#2c3e50']
+
+document.onload = set();
+
+function set() {
+  var count = Math.floor(Math.random()*quotes.length);
+  document.getElementById("quote").innerHTML = quotes[count][1];
+  document.getElementById("wik").innerHTML = quotes[count][0];
+  document.getElementsByClassName("button")[0].style.backgroundColor = colors[count];
+  document.getElementsByClassName("button")[1].style.backgroundColor = colors[count];
+  document.getElementsByClassName("button")[2].style.backgroundColor = colors[count];
+  document.getElementsByClassName("button")[3].style.backgroundColor = colors[count];
+  document.body.style.backgroundColor = colors[count];
+  document.body.style.color = colors[count];
+}
