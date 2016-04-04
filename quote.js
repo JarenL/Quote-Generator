8ddba8b224bfdb3ab8be18848caa0449a00a0c24
Jaren831 <jaren.lynchmobb@gmail.com>
2016-04-03 21:07:35 -0700

    Adds random-quote constructor - Adds constructor with quote, color, author, link attributes - Need to figure out how to shorten code for setting button colors

--- quote.js
+++ quote.js
@@ -12,21 +12,27 @@ var quotes = [
 ];
 var colors = ['#F44336', '#E91E63', '#7B1FA2', "#1A237E", "#90CAF9", "#1DE9B6", "#8BC34A", "#9E9E9E", "#607D8B", "#FF6E40"];
 
+function random_quote(quote, color, author, link) {
+	this.quote = quote; 
+	this.author = author; 
+	this.color = color;
+	this.link = link;
+};
+
 function set() {
 	var quoteSet = Math.floor(Math.random()*quotes.length);
 	var colorSet = Math.floor(Math.random()*colors.length);
-	document.getElementById("quote").innerHTML = quotes[quoteSet][1];
+	var pageSet = new random_quote(quotes[quoteSet][1], colors[colorSet], quotes[quoteSet][0], quotes[quoteSet][2]);
+	document.getElementById("quote").innerHTML = pageSet.quote;
 	//document.getElementById("pic").src = "https://upload.wikimedia.org/wikipedia/commons/b/b8/C%C3%A9sar_%2813667960455%29.jpg";
-	document.getElementById("author").innerHTML = "- " + quotes[quoteSet][0];
-	document.getElementById("wiki").href = quotes[quoteSet][2];
-	document.getElementById("tweet-quote").href = "https://twitter.com/intent/tweet?text=" + quotes[quoteSet][1] + "- " + quotes[quoteSet][0];
+	document.getElementById("author").innerHTML = "- " + pageSet.author;
+	document.getElementById("wiki").href = pageSet.link;
+	document.getElementById("tweet-quote").href = "https://twitter.com/intent/tweet?text=" + pageSet.quote + "- " + pageSet.author;
 	document.getElementById("fb-quote").href = "https://www.facebook.com/";
-	document.getElementsByClassName("btn")[0].style.backgroundColor = colors[colorSet];
-	document.getElementsByClassName("btn")[1].style.backgroundColor = colors[colorSet];
-	document.getElementsByClassName("btn")[2].style.backgroundColor = colors[colorSet];
-	document.getElementsByClassName("btn")[3].style.backgroundColor = colors[colorSet];
-	document.body.style.backgroundColor = colors[colorSet];
-	document.body.style.color = colors[colorSet];
+	document.body.style.backgroundColor = pageSet.color;
+	for (var i = 0; i < 2; i++) {
+		document.getElementsByClassName("btn")[i].backgroundColor = pageSet.color;
+	};
 };
 document.onload = set();
 document.getElementById("new").onclick = set();
